024da949d66e76701539d1ae80c2bd195b8d773c
Ali husnain <ahu8165@gmail.com>
2023-10-10 12:26:27 +0500

    Swap the vairable value

---
+++ swap_variable.py
@@ -0,0 +1,12 @@
+# Swap the two variable value 
+while True:
+    # Take first variable value
+    variable_1 = int(input("Enter the Value :"))
+    # Take first variable value
+    variable_2 = int(input("Enter the Value :"))
+    # Swap value of variable
+    Swap_value = variable_1
+    variable_1 = variable_2
+    variable_2 = Swap_value
+
+    print(f"Swap the First Variable  {Swap_value} With the Second Vairalbe {variable_1} : ",variable_1,variable_2)
