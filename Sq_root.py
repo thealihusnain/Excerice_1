df4f2df94ac68a84b171f8e51205d92ff8b9d871
Ali husnain <ahu8165@gmail.com>
2023-10-10 11:56:00 +0500

    Take requre root of number

---
+++ Sq_root.py
@@ -0,0 +1,12 @@
+# Write a program to take the sequre root of the number
+
+
+while True:
+    # Take input from user
+    Number = int(input("Write a number to find sequre root :"))
+    if(Number == 0 ):
+        break
+# Take sq_root
+    sq_root = Number ** 0.5
+# disply the Sequre Root 
+    print(sq_root)
