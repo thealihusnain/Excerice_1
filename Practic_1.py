88f02b15843b57753a844aa0c2c22807e5aed4d1
Ali husnain <ahu8165@gmail.com>
2023-10-10 11:39:10 +0500

    Sum of two value

---
+++ Practic_1.py
@@ -0,0 +1,8 @@
+# Declear first variable 
+var1 =10
+# Declear second variable 
+var2 =20
+# Add the two variable 
+sum = var1+var2
+# display the sum of two variable
+print("Sum of two variable :",sum)
