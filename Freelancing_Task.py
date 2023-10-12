20478990dbfd0a9ff2c3235aacb3a27920ad1a0d
Ali husnain <70138042+thealihusnain@users.noreply.github.com>
2023-10-12 16:47:03 +0500

    Update Freelancing_Task.py

---
+++ Freelancing_Task.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Step 1: Load the data from the provided Excel files
+mentors_df = pd.read_excel('C:\Personal Work\Freelancing_Task\Mentee final.xlsx')
+mentees_df = pd.read_excel('C:\Personal Work\Freelancing_Task\mentor final.xlsx')
+
+print(mentors_df,mentees_df)
+# Step 2: 
